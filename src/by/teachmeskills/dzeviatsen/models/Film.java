94f5c69ac0c9d01a8d4e0bc7e6bce4550871f5b8
Daniil Dzeiatsen <deviat.dan@gmail.com>
2023-04-15 11:43:37 +0300

    with sorting and filters with remarks from hw25

--- src/by/teachmeskills/dzeviatsen/models/Film.java
+++ src/by/teachmeskills/dzeviatsen/models/Film.java
@@ -10,6 +10,11 @@ public class Film extends Show {
     private final int numOfVotes;
 
     public Film(String name, int productionYear, String country, BigDecimal rating, int numOfVotes) {
+        validateName(name);
+        validateRating(rating);
+        validateYear(productionYear);
+        validateCountry(country);
+        validateNumOfVotes(numOfVotes);
         this.name = name;
         this.productionYear = productionYear;
         this.country = country;
@@ -40,6 +45,37 @@ public class Film extends Show {
     @Override
     public int getNumOfVotes() {
         return numOfVotes;
+    }
+
+    public static void validateName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Empty name");
+        }
+    }
+
+    public static void validateYear(int year) {
+        if (year < 0 || year > 2023) {
+            throw new IllegalArgumentException("Year must be positive");
+        }
+    }
+
+    public static void validateRating(BigDecimal rating) {
+        if (rating.compareTo(BigDecimal.ZERO) == -1
+                || rating.compareTo(BigDecimal.TEN) == 1) {
+            throw new IllegalArgumentException("Rating must have value between 0  and 10");
+        }
+    }
+
+    public static void validateCountry(String countryCode) {
+        if (countryCode.length() != 2) {
+            throw new IllegalArgumentException("Country code must contain 2 letters");
+        }
+    }
+
+    public static void validateNumOfVotes(int numOfVotes) {
+        if (numOfVotes < 0) {
+            throw new IllegalArgumentException("Number of votes must be over -1");
+        }
     }
 
     public String toString() {
--- src/by/teachmeskills/dzeviatsen/models/Serial.java
+++ src/by/teachmeskills/dzeviatsen/models/Serial.java
@@ -13,6 +13,14 @@ public class Serial extends Show {
     private final int numOfVotes;
 
     public Serial(String name, int productionYear, String country, BigDecimal rating, int numOfVotes, int lastEpisodeYear, int numOfEpisodes, int numOfSeasons) {
+        validateName(name);
+        validateRating(rating);
+        validateYear(productionYear);
+        validateCountry(country);
+        validateNumOfVotes(numOfVotes);
+        validateNumOfVotes(lastEpisodeYear);
+        validateNumOfSeasonsAndEpisodes(numOfEpisodes);
+        validateNumOfSeasonsAndEpisodes(numOfSeasons);
         this.name = name;
         this.productionYear = productionYear;
         this.country = country;
@@ -58,6 +66,43 @@ public class Serial extends Show {
 
     public int getNumOfSeasons() {
         return numOfSeasons;
+    }
+
+    public static void validateName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Empty name");
+        }
+    }
+
+    public static void validateYear(int year) {
+        if (year < 0 || year > 2023) {
+            throw new IllegalArgumentException("Year must be positive");
+        }
+    }
+
+    public static void validateRating(BigDecimal rating) {
+        if (rating.compareTo(BigDecimal.ZERO) == -1
+                || rating.compareTo(BigDecimal.TEN) == 1) {
+            throw new IllegalArgumentException("Rating must have value between 0  and 10");
+        }
+    }
+
+    public static void validateCountry(String countryCode) {
+        if (countryCode.length() != 2) {
+            throw new IllegalArgumentException("Country code must contain 2 letters");
+        }
+    }
+
+    public static void validateNumOfVotes(int numOfVotes) {
+        if (numOfVotes < 0) {
+            throw new IllegalArgumentException("Number of votes must be over -1");
+        }
+    }
+
+    public static void validateNumOfSeasonsAndEpisodes(int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("Number of episodes/seasons must be over 0");
+        }
     }
 
     public String toString() {
